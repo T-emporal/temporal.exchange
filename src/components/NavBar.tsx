b954b93bc8e2d2b560b359cc5e224fb0bbd3153e
Sayan Biswas <sayan@asia.com>
2023-03-16 14:15:31 +0530

    add grey border to modal

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -50,7 +50,7 @@ const NavBar: NextPage = () => {
                         >
                             <div className="relative w-auto my-6 mx-auto max-w-3xl">
                                 {/*content*/}
-                                <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-black text-gray-100 outline-none focus:outline-none">
+                                <div className="border-4 border-slate-500 rounded-lg shadow-3xl relative flex flex-col w-full bg-black text-gray-100 outline-none focus:outline-none">
                                     {/*header*/}
                                     {/* <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                                             <h3 className="text-3xl font-semibold">

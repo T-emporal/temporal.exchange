ead18deda5d5d1298bebc88fc0dfdc678ee8f481
Sayan Biswas <sayan@asia.com>
2023-03-16 14:50:06 +0530

    serve image from local

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="bg-fixed bg-cover bg-center bg-no-repeat" style={{
-        backgroundImage: "url('https://static.wixstatic.com/media/c1b522_ab3c6afea5c04905a84bd2e01b0463a5~mv2.jpeg/v1/fill/w_1240,h_929,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/c1b522_ab3c6afea5c04905a84bd2e01b0463a5~mv2.jpeg')",
+        backgroundImage: "url('background.webp')",
       }}>
         {/* <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]"> */}
         <Hero />

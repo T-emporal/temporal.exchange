e9ee226f486bec0c455c4e458fd06eea90978fb2
Jayraj Roshan <jayrajroshan1@gmail.com>
2024-02-21 19:12:58 +0530

    Added Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import Layout from "~/components/Layout";
 import "~/styles/globals.css";
+import { GoogleTagManager, GoogleAnalytics } from '@next/third-parties/google'
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <Layout>
       <Component {...pageProps} />
+      <GoogleTagManager gtmId="GTM-KRVCD2GP" />
+      <GoogleAnalytics gaId="G-JN63Q35Z7F" />
     </Layout>
   );
 };
